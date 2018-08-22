4e922721667030ef990a3ff7ee8d38202ecd61f9
Noah Gribbin <noahgribin@yahoo.com>
2018-08-22 14:56:11 -0700

    /getFinancial route scafold

---
+++ src/main/java/investment/Investment.java
@@ -0,0 +1,12 @@
+package investment;
+
+public class Investment {
+  private final int investmentAmmount;
+  private final int yearlyIncome;
+
+  public Investment(int investmentAmmount, int yearlyIncome) {
+    this.investmentAmmount = investmentAmmount;
+    this.yearlyIncome = yearlyIncome;
+  }
+
+}
---
+++ src/main/java/investment/InvestmentController.java
@@ -0,0 +1,15 @@
+package investment;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InvestmentController {
+    @RequestMapping(path="/getFinancials")
+    public Investment getFinancials(
+      @RequestParam(value="investmentAmmount", defaultValue=500000.00) int investmentAmmount,
+      @RequestParam(value="yearlyIncome", defaultValue=2500.00) int yearlyIncome) {
+        return new Investment(investmentAmmount, yearlyIncome);
+      }
+}
